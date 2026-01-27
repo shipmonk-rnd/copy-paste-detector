5c37712e614a718dccb79558727a80a503f763ea
Jan Nedbal <jan.nedbal@shipmonk.com>
2026-01-27 17:38:47 +0100

    Remove unused origNode attribute from CloningVisitor

--- src/Hashing/CloningVisitor.php
+++ src/Hashing/CloningVisitor.php
@@ -6,19 +6,16 @@ use PhpParser\Node;
 use PhpParser\NodeVisitorAbstract;
 
 /**
- * Visitor cloning all nodes and linking to the original nodes using an attribute.
+ * Visitor that deep clones all AST nodes.
  *
- * This visitor is required to perform format-preserving operations and
- * is more efficient than serialize/unserialize for deep cloning AST nodes.
+ * More efficient than serialize/unserialize for deep cloning AST nodes.
  */
 final class CloningVisitor extends NodeVisitorAbstract
 {
 
     public function enterNode(Node $node): Node
     {
-        $clonedNode = clone $node;
-        $clonedNode->setAttribute('origNode', $node);
-        return $clonedNode;
+        return clone $node;
     }
 
 }
