a6af6a11bfef9946d6d6e4229b17add758a573d5
Jan Nedbal <jan.nedbal@shipmonk.com>
2026-01-27 10:55:30 +0100

    Adjust own config (example is in readme)

--- copy-paste-detector.php
+++ copy-paste-detector.php
@@ -3,30 +3,9 @@
 use CopyPasteDetector\Config\Config;
 
 $config = new Config();
-
-// Set paths to analyze
 $config->setPaths(['src', 'tests']);
 $config->setExcludePaths(['tests/_fixtures']);
-
-// Set cache directory
 $config->setCacheDir(__DIR__ . '/cache/self');
-
-// Set the minimum node count for clone detection
-// Higher values = fewer but larger clones detected
 $config->setMinNodeCount(30);
-
-// Enable or disable different anonymization strategies:
-
-// Variables: When true, $foo and $bar are considered equivalent (default: true)
-$config->setAnonymizeVariables(true);
-
-// Literals: When true, "hello" and "world" or 1 and 2 are considered equivalent (default: false)
-$config->setAnonymizeLiterals(false);
-
-// Names: When true, function/class names are considered equivalent (default: false)
-$config->setAnonymizeNames(false);
-
-// Identifiers: When true, method/constant names are considered equivalent (default: false)
-$config->setAnonymizeIdentifiers(false);
 
 return $config;
